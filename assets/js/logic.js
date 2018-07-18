cdf177acb64084fad3d3f13605771863d9b6d39a
Mack <ryanmcilvenna@Ryans-MacBook-Pro.local>
2018-07-18 16:20:14 -0700

    added many things

---
+++ assets/js/logic.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+
+
+
+$( ".submit" ).click(function() {
+    swal("Good job!", "You clicked the button!", "success")
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
